2aad7c5ee101f1aa1f655c209bac0cf89cc4a31d
AMohajerani1998 <ashkan.mohajerani1998@gmail.com>
2024-05-27 13:39:10 +0200

    finished Update and Delete methods on tasks.

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 const TodoApp = {
     data() {
         return {
+            isLoading: false,
             enteredTask: "",
             todos: [],
             editedTodoId: null,
@@ -8,22 +9,50 @@ const TodoApp = {
     },
 
     methods: {
-        saveTodo(event) {
+        async saveTodo(event) {
             event.preventDefault();
             if (this.editedTodoId) {
+                let response;
                 const todoId = this.editedTodoId;
-                const todoEditIndex = this.todos.findIndex(function (todoItem) {
-                    return todoItem.id === todoId;
-                });
+                try {
+                    response = await fetch (`http://localhost:3000/todos/${todoId}`, {
+                        method: 'PATCH',
+                        body: JSON.stringify({
+                            task: this.enteredTask,
+                        }),
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                } catch (error) {
+                    return alert ('Something went wrong!')
+                }
+                if (!response.ok){
+                    return alert ('Something went wrong!')
+                }
+                const responseData = await response.json()
+                const todoEditIndex = this.todos.findIndex(function(todo){
+                    return todo.id === responseData.todo.id
+                })
                 const todoEdit = {
                     id: this.todos[todoEditIndex].id,
                     task: this.enteredTask,
                 };
                 this.todos[todoEditIndex] = todoEdit;
             } else {
+                const result = await fetch("http://localhost:3000/todos", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        task: this.enteredTask,
+                    }),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                const resultData = await result.json();
                 const newTodo = {
-                    task: this.enteredTask,
-                    id: new Date().toISOString(),
+                    task: resultData.todo.task,
+                    id: resultData.todo.id,
                 };
                 this.todos.unshift(newTodo);
             }
@@ -38,11 +67,39 @@ const TodoApp = {
             this.enteredTask = todo.task;
         },
 
-        deleteTodo(todoId){
-            this.todos = this.todos.filter(function(todoItem){
-                return todoItem.id !== todoId
-            })
+        async deleteTodo(todoId) {
+            let result;
+            try {
+                result = await fetch (`http://localhost:3000/todos/${todoId}`, {
+                    method: 'DELETE'
+                })
+            } catch (error) {
+                return alert ('Something went wrong!')
+            }
+            if (!result.ok) {
+                return alert ('Something went wrong!')
+            }
+            this.todos = this.todos.filter(function (todoItem) {
+                return todoItem.id !== todoId;
+            });
+        },
+    },
+
+    async created() {
+        this.isLoading = true;
+        let result;
+        try {
+            result = await fetch("http://localhost:3000/todos");
+        } catch (error) {
+            this.isLoading = false;
+            return alert("Something went wrong!");
         }
+        this.isLoading = false;
+        if (!result.ok){
+            return alert ('Something went wrong!')
+        }
+        resultData = await result.json();
+        this.todos = resultData.todos
     },
 };
 
